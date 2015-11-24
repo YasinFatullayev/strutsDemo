f779228ee15d13de926e67b1bedaf34dd3d7d64f
YasinFatullayev <yasin.fatullayev@gmail.com>
2015-11-23 21:33:40 -0500

    demo

---
+++ SpringStrutsHibernate/src/test/java/testP/AppTest.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package testP;
+
+import junit.framework.TestCase;
+
+/**
+ * @author yasin
+ *
+ */
+public class AppTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public AppTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#getId()}.
+	 */
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#setId(int)}.
+	 */
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#getName()}.
+	 */
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#setName(java.lang.String)}.
+	 */
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#getNickname()}.
+	 */
+	public void testGetNickname() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#setNickname(java.lang.String)}.
+	 */
+	public void testSetNickname() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#getPassword()}.
+	 */
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.RegAndLog#setPassword(int)}.
+	 */
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+}
